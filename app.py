2ce0b913143faf91da77c5ae834063cf14fba9ce
VENNAPUSA GIRIVARDHAN REDDY <135786118+giri521@users.noreply.github.com>
2025-09-17 10:56:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,6 @@ def predict():
 
     except Exception as e:
         return jsonify({"error": str(e)})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment, default to 5000
+    app.run(host="0.0.0.0", port=port)
